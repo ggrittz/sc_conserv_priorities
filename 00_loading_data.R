00a0cf82961b47085b750b192069dd825c756237
Guilherme S. Grittz <45982618+ggrittz@users.noreply.github.com>
2024-04-22 19:11:05 -0300

    Update 00_loading_data.R
    .

--- 00_loading_data.R
+++ 00_loading_data.R
@@ -25,7 +25,12 @@
 
 #Sites and environmental data
 sites <- readRDS('rds/pa_common.rds')
+sites <- terra::unwrap(sites)
+
+#This is a site-by-species matrix of 0s and 1s
 sites <- terra::as.data.frame(sites, xy = TRUE, geom = 'XY')
+
+#Raster cells over the study area (Santa Catarina)
 env_terra <- readRDS('rds/predictors_df.rds') %>% rast()
 env_raster <- brick(env_terra) #for blockCV
 #env <- terra::as.data.frame(env, xy = TRUE, geom = 'XY')
